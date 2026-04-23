aa0f463e870c223dec3503e9c46ef6742db116cc
Akshay Pant <akshay.akshaypant@gmail.com>
2026-04-23 18:55:52 +0530

    feat: add k8s manifests and controller health probes
    Namespace, RBAC, deployment and service manifests
    for controller and webhook. Health and readiness
    endpoints wired up on the controller.
    
    Signed-off-by: Akshay Pant <akshay.akshaypant@gmail.com>

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -8,6 +8,7 @@ import (
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
@@ -47,6 +48,15 @@ func main() {
 	reconciler := repository.NewReconciler(mgr.GetClient(), builder)
 	if err := reconciler.SetupWithManager(mgr); err != nil {
 		logger.Error(err, "unable to set up Repository controller")
+		os.Exit(1)
+	}
+
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		logger.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		logger.Error(err, "unable to set up ready check")
 		os.Exit(1)
 	}
 
